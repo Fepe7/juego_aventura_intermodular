02b101388ed2fab4d27eb224656a38bd15b929f8
XGM12 <xg_mosaic@proton.me>
2025-09-30 18:07:37 +0200

    adición del public al psvm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         //Creas los 4 personajes iniciales
         Personaje personaje1 = Personaje.CrearPersonaje();
