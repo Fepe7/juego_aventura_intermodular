657509b8d6534343a9b5a63b2b679f8add7eb582
Fepe7 <andreistaicu568@gmail.com>
2025-10-01 13:42:51 +0200

    1/10/25 13:16 Falta que los personajes se muera, que cuadno mates al dragon acabe la partida.

--- src/main/java/Combates.java
+++ src/main/java/Combates.java
@@ -9,9 +9,9 @@ public class Combates {
 
     //Devuelve el enemigo con el que se va a combatir segun la ronda en la que estes
     public static Enemigos combate(int ronda){
-        Enemigos goblin = new Enemigos( "Goblin", 150, 20, 10, "Un goblin salvaje" );
-        Enemigos orco = new Enemigos( "Orco", 250, 25, 5, "Un orco feroz" );
-        Enemigos troll = new Enemigos( "Troll", 300, 30, 3, "Un troll gigante" );
+        Enemigos goblin = new Enemigos( "Goblin", 150, 25, 10, "Un goblin salvaje" );
+        Enemigos orco = new Enemigos( "Orco", 250, 30, 5, "Un orco feroz" );
+        Enemigos troll = new Enemigos( "Troll", 300, 35, 3, "Un troll gigante" );
         Enemigos dragon = new Enemigos( "Dragón", 400, 50, 25, "Un dragón aterrador" );
         Enemigos lobo = new Enemigos( "Lobo", 80, 20, 15, "Un lobo salvaje" );
 
