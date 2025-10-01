86977f5d003f64b6764fb8580a0f1c12f4f8e45b
sanherhua <santiherra06@gmail.com>
2025-10-01 09:57:07 +0200

    Santi - 9:57

--- src/main/java/ImportarDatos.java
+++ src/main/java/ImportarDatos.java
@@ -7,8 +7,8 @@ public class ImportarDatos {
     public static void main(String[] args) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-        try (FileReader reader = new FileReader("jugador.json")) {
-             Personaje personaje = gson.fromJson(reader, Personaje.class);
+        try (FileReader reader_jugador = new FileReader("partida.json")) {
+            Personaje personaje = gson.fromJson(reader_jugador, Personaje.class);
 
             System.out.println("Jugador cargado desde JSON:");
             System.out.println("Nombre: " + personaje.getNombre());
@@ -19,8 +19,17 @@ public class ImportarDatos {
             System.out.println("Mana: " + personaje.getMana());
             System.out.println("Vivo: " + personaje.isVivo());
         } catch (IOException e) {
-            System.out.println("Error al leer el archivo JSON: " +   e.getMessage());
+            System.out.println("Error al leer el archivo JSON: " + e.getMessage());
 
+        }
+
+        try (FileReader reader_inventario = new FileReader("objetos.json")) {
+            InventarioGlobal inventarioGlobal = gson.fromJson(reader_inventario, InventarioGlobal.class);
+
+            System.out.println("Inventario cargado desde JSON:");
+            System.out.println("Inventario: " + inventarioGlobal);
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo JSON: " + e.getMessage());
         }
     }
 }
