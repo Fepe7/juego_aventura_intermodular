e2f2c77ac1991de4443d79e14ea592078ed009e0
sanherhua <santiherra06@gmail.com>
2025-10-02 12:28:09 +0200

    Santi - 12:27 - Todos los TextConsole

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,11 +13,13 @@ public class Main {
 
             try {
                 do {
+                    TextConsoleWindow.printDivider();
                     System.out.println("""
                         Se ha detectado una partida guardada.
                         1) Importar partida guardada
                         2) Crear una nueva partida
                         Escoge una opción (1-2): """);
+                    TextConsoleWindow.printDivider();
 
                     opcion = scanner.nextInt();
 
@@ -33,9 +35,12 @@ public class Main {
                 personajesPartida = ImportarDatos.cargarPersonajes();
                 if (personajesPartida == null || personajesPartida.length == 0) {
                     System.out.println("No se ha podido importar la partida. Se creará una nueva partida.");
+                    TextConsoleWindow.printDivider();
                     personajesPartida = crearPartidaNueva();
                 } else {
+                    TextConsoleWindow.printBanner("Partida importada");
                     System.out.println("Partida importada correctamente.");
+                    TextConsoleWindow.printDivider();
                     var objetos = ImportarDatos.cargarObjetos();
                     if ( objetos != null ) {
                         InventarioGlobal.setInventarioGlobal(objetos);
@@ -43,13 +48,15 @@ public class Main {
                     } else {
                         System.out.println("No se han podido importar los objetos. Se usarán los objetos iniciales.");
                     }
+                    TextConsoleWindow.printDivider();
                 }
             } else {
                 personajesPartida = crearPartidaNueva();
             }
         } else {
-            System.out.println("No se ha encontrado ninguna partida guardada. Se creará una nueva partida.");
+            TextConsoleWindow.printBanner("No se ha encontrado ninguna partida guardada. Se creará una nueva partida.");
             personajesPartida = crearPartidaNueva();
+            TextConsoleWindow.printDivider();
         }
 
         ArrayList<Objeto> inventarioGlobal = new ArrayList<>(InventarioGlobal.getInventarioGlobal());
@@ -57,24 +64,29 @@ public class Main {
         int ronda = 0;
 
         while (personajesPartida[0].isVivo() || personajesPartida[1].isVivo() || personajesPartida[2].isVivo() || personajesPartida[3].isVivo()) {
+            TextConsoleWindow.printBanner("Ronda " + (ronda + 1));
             Enemigos enemigo = Combates.combate(ronda);
             System.out.println("¡Un " + enemigo.getNombre() + " ha aparecido! Prepárate para la batalla.");
+            TextConsoleWindow.printDivider();
             while (enemigo.isVivo() && (personajesPartida[0].isVivo() || personajesPartida[1].isVivo() || personajesPartida[2].isVivo() || personajesPartida[3].isVivo())) {
                 Juego.turnoActual(personajesPartida, enemigo);
+                TextConsoleWindow.printDivider();
             }
 
             if (!enemigo.isVivo()) {
                 //Aqui iria lo de pasar de habitacion y toda la pesca
-                System.out.println("\n=== VICTORIA! ===");
+                TextConsoleWindow.printBanner("\n=== VICTORIA! ===");
                 System.out.println("\n¿Quieres guardar partida (S/N) ?\n");
                 String respuesta = scanner.next();
                 if ("s".equalsIgnoreCase(respuesta)) {
                     guardadPartida(personajesPartida, inventarioGlobal);
+                    TextConsoleWindow.printDivider();
                 }
 
                 ronda++;
 
                 SistemaPostBatalla.opcionesPostBatalla(personajesPartida, inventarioGlobal);
+                TextConsoleWindow.printDivider();
             } else {
                 System.out.println("Todos tus personajes han muerto. Fin del juego.");
                 break;
