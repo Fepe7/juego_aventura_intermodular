59112a0a0e9d0ba4219cd152ad12e9709c1c94d4
XGM12 <xg_mosaic@proton.me>
2025-10-30 13:05:06 +0100

    Mejora y expansión de comentarios de código

--- src/main/java/Juego_nuevo/persistencia_datos_JSON/estadoJugador.java
+++ src/main/java/Juego_nuevo/persistencia_datos_JSON/estadoJugador.java
@@ -1,4 +1,6 @@
-
+/**
+ * @author Xavier Gutiérrez Martínez
+ */
 
 package Juego_nuevo.persistencia_datos_JSON;
 
@@ -13,18 +15,19 @@ import java.util.List;
 
 public class estadoJugador {
     /**
-     * La función createGson() crea el objeto GSON con prettyPrinting.
-     * @return el Objeto GSON
+     * La función <strong>{@code createGson()}</strong> crea el objeto {@link Gson} con {@link GsonBuilder} para obtener su
+     * función {@code setPrettyPrinting()} el cual permite mostrarlo de forma ordenada y bonita.
+     * @return  el objeto {@link Gson}
      */
-    private static Gson createGson() {
-        return new GsonBuilder().setPrettyPrinting().create();
-    }
+    private static Gson createGson() { return new GsonBuilder().setPrettyPrinting().create(); }
 
     /**
-     * La función <strong>guardarPartida()</strong> guarda la partida actual, contando los personajes, objetos y la ubicación del mapa
+     * La función <strong>guardarPartida()</strong> guarda la partida actual, contando los personajes, objetos y la
+     * ubicación del mapa
      * <p>
-     * Esta función usa el objeto GSON, y después en el try with resources, donde declaramos el FileWriter con fichero
-     * de partida.json, y guardamos las listas pasadas. Si no, tira una excepción
+     * Esta función usa el objeto {@link Gson}. Dentro del {@code try-catch-with-resources} declaramos {@link FileWriter}
+     * con fichero de <strong>partida.json</strong>, y guardamos las listas pasadas. Si no, lanza una excepción de tipo {@link Exception}
+     * el cual saca del programa de forma fatal.
      * @param personajes La lista de personajes que hay
      * @param objeto La lista de objetos que tienen globalmente todos
      */
@@ -38,6 +41,15 @@ public class estadoJugador {
         }
     }
 
+    /**
+     * La función <strong>{@code cargarPersonajes()}</strong> carga los personajes
+     * <p>
+     * Esta función usa el objeto {@link Gson}. Dentro del {@code try-catch-with-resources} declaramos {@link FileWriter}
+     * y le pasamos el archivo de origen, el cual es <strong>partida.json</strong> y devuelve el resultado de
+     * la función {@code gson.fromJson(FileWriter, Personaje[].class)}, lo cual lee la línea y mete el objeto en
+     * el array de Personaje. Si no, lanza una excepción de tipo {@link Exception}, devolviendo null
+     * @return La lista de personajes en forma de array | null
+     */
     public static Personaje[] cargarPersonajes() {
         final var gson = createGson();
         try (final var fr = new FileReader("partida.json")) {
@@ -49,8 +61,8 @@ public class estadoJugador {
     }
 
 
-//    public static List<Objeto> cargarObjetos() {
-//
-//    }
+    public static List<Objeto> cargarObjetos() {
+        return null;
+    }
 
 }
