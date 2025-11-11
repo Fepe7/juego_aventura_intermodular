f266605533695f2be82428ee8a50f62ec09a954c
Fepe7 <andreistaicu568@gmail.com>
2025-11-11 12:59:03 +0100

    Añadidometodos de habilidades que no se ejecutaban mediante reflexion

--- src/main/java/Juego_nuevo/Habilidad.java
+++ src/main/java/Juego_nuevo/Habilidad.java
@@ -21,16 +21,40 @@ public class Habilidad {
         this.tipoHabilidad = tipo;
     }
 
-public void bendicionElfa(Personaje p) {
-    p.setVida(p.getVida() + 15);
-    p.setAtaque(p.getAtaque() + 5);
-    Objeto.comprobarVidaMaxima(p);
-    System.out.println("Has usado " + nombre);
-    System.out.println("Has sido bendecido: +15 vida y +5 ataque");
-}
+    public void bendicionElfa(Personaje p) {
+        p.setVida(p.getVida() + 15);
+        p.setAtaque(p.getAtaque() + 5);
+        Objeto.comprobarVidaMaxima(p);
+        System.out.println("Has usado " + nombre);
+        System.out.println("Has sido bendecido: +15 vida y +5 ataque");
+    }
 
 
+    public void ataqueRapido(Personaje p, Enemigo e) {
+        int vidaQuitar = 15 + p.getAtaque();
+        e.setVida(e.getVida() - vidaQuitar);
+        System.out.println("Has usado " + nombre);
+        System.out.println("Has inflingido " + vidaQuitar + " puntos de daño a " + e.getNombre());
+    }
 
+    public void golpeMortal(Personaje p, Enemigo e) {
+        int vidaQuitar = 30 + p.getAtaque();
+        e.setVida(e.getVida() - vidaQuitar);
+        System.out.println("Has usado " + nombre);
+        System.out.println("Has inflingido " + vidaQuitar + " puntos de daño a " + e.getNombre());
+    }
+
+    public void escudoMagico(Personaje p) {
+        p.setAtaque(p.getAtaque() + 8);
+        System.out.println("Has usado " + nombre);
+        System.out.println("Escudo mágico activado: +8 de defensa temporal");
+    }
+
+    public void gritoDGuerra(Personaje p) {
+        p.setAtaque(p.getAtaque() + 10);
+        System.out.println("Has usado " + nombre);
+        System.out.println("¡Grito de guerra! Tu ataque aumentó en 10 puntos");
+    }
 
     public void golpePoderoso(Personaje p, Enemigo e) {
         int vidaQuitar = 15 + p.getAtaque();
